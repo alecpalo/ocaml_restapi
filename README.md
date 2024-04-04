5ffee3d8c328e798ccdbc13a7ac896c6f0db2449
Alec <alecpalo@Alecs-MacBook-Pro-4.local>
2024-04-03 22:37:45 -0400

    made the class, need to finish api calls

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# ocaml_restapi
+# ocaml restapi
+
+To run:
+dune exec -- ocaml_restapi
+
+## What is this?
+I want to learn OCaml and I want to brush up on restapis.
+
+This is going to be a restapi todo list where you can Post todos, get the list of todos, and remove todos
+
+
--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,41 @@
-let () = print_endline "Hello, World!"
+open Opium
+
+class task (title : string) (description : string) (deadline : string) = object
+  val mutable title = title
+  val mutable description = description
+  val mutable deadline = deadline
+  
+  method getTitle = title
+  method setTitle newTitle = title <- newTitle
+
+  method getDescription = description
+  method setDescription newDescription = description <- newDescription 
+
+  method getDeadline = deadline
+  method setDeadline newDeadline = deadline <- newDeadline
+end;;
+
+
+let tasks : task list = [];;
+
+let getList _req = 
+  Response.of_plain_text |> Lwt.return
+
+let addTask req =
+    let name = Router.param req "name" in
+    Printf.sprintf "Hello, %s" name |> Response.of_plain_text |> Lwt.return
+
+let removeTask req =
+  let task = Router.param req "task" in 
+  let temp = `Assoc [
+    ("deteled", `String task);
+  ] in
+  Response.of_json temp |> Lwt.return
+
+let () =
+    App.empty
+    |> App.get "/" getList 
+    |> App.get "/add/:name" addTask 
+    |> App.get "/remove/:task" removeTask 
+    |> App.run_command
+    |> ignore
